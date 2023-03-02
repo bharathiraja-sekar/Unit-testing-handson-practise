08d948f5d2c90f37c5e13243f67a4e0701b200e7
bharathiraja-sekar <bharathiraja.s101@gmail.com>
2023-03-02 18:49:38 +0530

    Unit test cases

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -1,9 +1,54 @@
 import { html, fixture, expect } from '@open-wc/testing';
-import { stub } from 'sinon';
+import Sinon,{ stub } from 'sinon';
 import '../src/LoanBasicDetails/BasicDetails.js';
 
+const el = await fixture(html`<basic-details></basic-details>`);
 describe('Basic details', () => {
-  // Write test cases inside this block
-  // refer basic-details.js files
-  // test cases
+  it('Check for id', async() => {
+    const el = await fixture('<div id = "word"></div>');
+    expect(el.id).to.equal('word');
+});
+
+it('Checking for web accessebility', () => {
+  expect(el).to.be.exist;
+});
+
+it('Check for amount', async () => {
+    const el = await fixture(html`<basic-details></basic-details>`);
+    expect(el.amount).to.be.equal(10000);
+});
+
+it('Check the type for amount', async () => {
+    const el = await fixture(html`<basic-details></basic-details>`);
+    expect(el.amount).to.be.a('number');
+});
+
+it('Checking for element', async () => {
+    const el = await fixture(html`<lion-input .value="text"></lion-input>`);
+    expect(el.getAttribute(".value")).to.be.equal("text");
+});
+
+it('Checking for emiCalc', async () => {
+    const el = await fixture(html`<basic-details></basic-details>`);
+    expect(el.emiCalc).to.be.equal(0);
+});
+it('Checking captureDetails when clicked', async () => {
+  setTimeout(async () => {
+      const el = await fixture(html`<basic-details></basic-details>`);
+      const spy = sinon.spy(el._captureDetails);
+      el.requestUpdate();
+      await el.updateComplete;
+      el.shadowRoot.getElementById('lion-btn').click();
+      expect(spy.called).to.be.true;
+  }, 2000);
+});
+
+it('Checking toDashBoard when clicked', async () => {
+  const el = await fixture(html`<basic-details></basic-details>`);
+  const myFunctionStub = Sinon.stub(el, '_toDashboard');
+  el.requestUpdate();
+  await el.updateComplete;
+  el.shadowRoot.querySelector('lion-button').click();
+  expect(myFunctionStub).to.have.callCount(1);
+});
 });
--- test/loan-emi-details.test.js
+++ test/loan-emi-details.test.js
@@ -1,6 +1,19 @@
 import { html, fixture, expect } from '@open-wc/testing';
 import '../src/LoanEMIDetails/LoanEMIDetails.js';
 
+
+const el = await fixture(html`<loanemi-details></loanemi-details>`);
+const button = el.shadowRoot.querySelectorAll('button');
+
 describe('Loan EMI details', () => {
   // Write test cases inside this block
+  it('Accessible', async () => {
+    expect(el).to.be.accessible();
+});
+
+it('check for the h2 heading', async () => {
+    const h2 = el.shadowRoot.querySelector('h2');
+    expect(h2).to.exist;
+    expect(h2.textContent).to.equal('EMI Details');
+});
 });
